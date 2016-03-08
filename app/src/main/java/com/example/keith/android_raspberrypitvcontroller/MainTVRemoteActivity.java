42c9175fe9fa31f95cf44616080e06ce09611759
kwalker328@gmail.com <Gameofthrones4>
2016-03-08 13:11:07 +0000

    Scan for bluetooth button added with listeners

--- app/src/main/java/com/example/keith/android_raspberrypitvcontroller/MainTVRemoteActivity.java
+++ app/src/main/java/com/example/keith/android_raspberrypitvcontroller/MainTVRemoteActivity.java
@@ -1,6 +1,9 @@
 package com.example.keith.android_raspberrypitvcontroller;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +23,7 @@ public class MainTVRemoteActivity extends AppCompatActivity {
     private Button volumeDownButton;
     private Button channelUpButton;
     private Button channelDownButton;
+    private Button scanBluetoothBtn;
     private BluetoothAdapter bluetoothAdapter;
 
     @Override
@@ -30,7 +34,6 @@ public class MainTVRemoteActivity extends AppCompatActivity {
 
         //check tv on off state
         checkTVOnOffState();
-        checkBluetoothState();
 
         //set the switch to ON
         tvOnOffSwitch.setChecked(true);
@@ -39,9 +42,12 @@ public class MainTVRemoteActivity extends AppCompatActivity {
         volumeControls();
         channelControls();
 
+        //bluetooth scan button
+        onClickScanBluetooth();
+
     }
 
-    public void setUpUI(){
+    public void setUpUI() {
         //find interactive components on screen
         switchStatus = (TextView) findViewById(R.id.switchStatus);
         tvOnOffSwitch = (Switch) findViewById(R.id.tvOnOffSwitch);
@@ -49,10 +55,11 @@ public class MainTVRemoteActivity extends AppCompatActivity {
         volumeDownButton = (Button) findViewById(R.id.volumeDownButton);
         channelUpButton = (Button) findViewById(R.id.channelUpButton);
         channelDownButton = (Button) findViewById(R.id.channelDownButton);
+        scanBluetoothBtn = (Button) findViewById(R.id.scanBluetoothBtn);
 
     }
 
-    public void checkTVOnOffState(){
+    public void checkTVOnOffState() {
         //attach a listener to check for changes in state
         tvOnOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
@@ -69,19 +76,19 @@ public class MainTVRemoteActivity extends AppCompatActivity {
 
     }
 
-    public void volumeControls(){
+    public void volumeControls() {
         //volume up listener
         View.OnClickListener oclVolumeUpBtn = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainTVRemoteActivity.this, "Volume Up",100).show();
+                Toast.makeText(MainTVRemoteActivity.this, "Volume Up", 100).show();
             }
         };
         //volume down
         View.OnClickListener oclVolumeDownBtn = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainTVRemoteActivity.this, "Volume Down",100).show();
+                Toast.makeText(MainTVRemoteActivity.this, "Volume Down", 100).show();
             }
         };
         // assign click listener to buttons
@@ -90,11 +97,11 @@ public class MainTVRemoteActivity extends AppCompatActivity {
     }
 
 
-    public void channelControls(){
+    public void channelControls() {
         View.OnClickListener oclChannelUpBtn = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainTVRemoteActivity.this, "Channel Up",100).show();
+                Toast.makeText(MainTVRemoteActivity.this, "Channel Up", 100).show();
 
             }
         };
@@ -102,7 +109,7 @@ public class MainTVRemoteActivity extends AppCompatActivity {
         View.OnClickListener oclChannelDownBtn = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainTVRemoteActivity.this, "Channel Down",100).show();
+                Toast.makeText(MainTVRemoteActivity.this, "Channel Down", 100).show();
             }
         };
         // assign click listener to the OK button (btnOK)
@@ -111,28 +118,51 @@ public class MainTVRemoteActivity extends AppCompatActivity {
 
     }
 
-    public void checkBluetoothState(){
+    public boolean checkBluetoothState() {
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if(bluetoothAdapter.isEnabled()){
-            Toast.makeText(MainTVRemoteActivity.this, "Bluetooth is on",Toast.LENGTH_LONG).show();
-        }
-        else{
-            Toast.makeText(MainTVRemoteActivity.this, "Bluetooth is off",Toast.LENGTH_LONG).show();
+        if (bluetoothAdapter.isEnabled()) {
+            Toast.makeText(MainTVRemoteActivity.this, "Bluetooth is on", Toast.LENGTH_LONG).show();
+            return true;
+        } else {
+            Toast.makeText(MainTVRemoteActivity.this, "Bluetooth is off", Toast.LENGTH_LONG).show();
             turnOnBluetooth();
+            return false;
         }
 
     }
 
-    public void turnOnBluetooth(){
+    public void turnOnBluetooth() {
         String bluetoothStateChanged = BluetoothAdapter.ACTION_STATE_CHANGED;
         String bluetoothRequestEnable = BluetoothAdapter.ACTION_REQUEST_ENABLE;
         IntentFilter filter = new IntentFilter(bluetoothStateChanged);
         startActivityForResult(new Intent(bluetoothRequestEnable), 0);
 
+    }
+
+    public void onClickScanBluetooth() {
+
+        View.OnClickListener oclScanBluetoothBtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainTVRemoteActivity.this, "Scan for ",100).show();
+                if (checkBluetoothState() == true) {
+                    Toast.makeText(MainTVRemoteActivity.this, "Scanning...", 100).show();
+                    scanBluetooth();
+                }
+
+            }
+        };
+        // assign click listener to the OK button (btnOK)
+        scanBluetoothBtn.setOnClickListener(oclScanBluetoothBtn);
+    }
+
+    public void scanBluetooth() {
+
 
     }
 
-
 }
+
+
 
